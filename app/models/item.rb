6bb68600afda1441f2ae7ec3abf7377650696172
Airi-tech <airis1631@gmail.com>
2020-08-09 17:36:16 +0900

    Fix: itemsモデルの記述の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -28,6 +28,6 @@ class Item < ApplicationRecord
   end
 
   validates :price, presence: true, numericality:
-  { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
 
