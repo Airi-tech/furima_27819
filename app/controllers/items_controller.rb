c38794d4fe6390382024b21c75f7eee819a35fa4
Airi-tech <airis1631@gmail.com>
2020-08-09 14:45:13 +0900

    Add: itemsコントローラー　createアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,17 @@ class ItemsController < ApplicationController
   def new
     if user_signed_in?
       @item=Item.new
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to :root
     else
-      redirect_to new_user_session_path
+      render :new
+    end
+  end  
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id:current_user.id)
   end
 end
