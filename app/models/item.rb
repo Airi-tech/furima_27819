96d155eecef44e3a92a0f530aea628ba104ebbb6
Airi-tech <airis1631@gmail.com>
2020-08-09 13:18:51 +0900

    Add: itemsテーブルのアソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_fee_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
+  
+  belongs_to :user
+  has_many :comments
+  has_one :order
+  has_one :destination
+
 end
