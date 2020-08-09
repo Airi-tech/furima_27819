494ced8230222ce24737df6eebac9c707e25b82f
Airi-tech <airis1631@gmail.com>
2020-08-09 17:35:31 +0900

    Add: itemモデルの単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name                     { 'ボール' }
+    info                     { '要らなくなったので売ります' }
+    category_id              { '2' }
+    sales_status_id          { '2' }
+    shipping_fee_status_id   { '2' }
+    prefecture_id            { '2' }
+    scheduled_delivery_id    { '2' }
+    price                    { '300' }
+    association :user
   end
 end
