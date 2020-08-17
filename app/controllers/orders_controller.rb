5bd46d2c4e206c42ce03d414a461827fd73ef52b
Airi-tech <airis1631@gmail.com>
2020-08-18 01:33:22 +0900

    Add: ログインしていないユーザーが購入ページに遷移しようとすると、ログインページに遷移する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :move_to_login, only: [:index, :create]
 
   def index
     @item = Item.find(params[:item_id])
@@ -6,4 +7,14 @@ class OrdersController < ApplicationController
 
   def create
   end
+
+  private
+
+  def move_to_login
+    unless user_signed_in?
+      flash[:alert] = 'You need to sign in or sign up before continuing.'
+      redirect_to new_user_session_path
+    end
+  end
+
 end
